1e48eaa1458ca91d5623f6bf615deef293604c8c
albertitocode <halberto1407@gmail.com>
2025-01-12 21:38:47 -0500

    map

--- mapa/mapaRegistro.php
+++ mapa/mapaRegistro.php
@@ -1,4 +1,72 @@
 <style type="text/css">
+    .mscross {
+        border: 1px solid #7ea3bf;
+        /* color: #000000; */
+    }
+
+    .mscross_report_title {
+        color: #000000;
+    }
+
+    .mscross_report_attr_name {
+        color: #000000;
+        font-weight: normal;
+    }
+
+    .mscross_report_attr_value {
+        color: #115aa4;
+        font-weight: bold;
+    }
+
+    .mscross_reference_zoombox {
+        color: #115aa4;
+        background: #000000;
+        font-weight: bold;
+        border: 1px solid #000000;
+    }
+
+
+
+    .maintable {
+        border: 1px solid #7ea3bf;
+        /* maintable */
+    }
+
+
+    a#francobollo {
+        position: fixed;
+        left: 0;
+        top: 0;
+        display: block;
+        height: 80px;
+        width: 80px;
+        /* background: url(logo_sm_144x35.jpg) top left no-repeat; */
+        text-indent: -999em;
+        text-decoration: none;
+        z-index: 100;
+    }
+
+
+
+    TD A:hover {
+        BACKGROUND-COLOR: #ffffcc;
+    }
+
+    LAYER {
+        BORDER-RIGHT: #008080 thin inset;
+        BORDER-TOP: #008080 thin inset;
+        FONT-SIZE: 11px;
+        PADDING-BOTTOM: 5px;
+        BORDER-LEFT: #33aaaa thin inset;
+        PADDING-TOP: 5px;
+        BORDER-BOTTOM: #33aaaa thin inset;
+        FONT-STYLE: normal;
+        FONT-FAMILY: Arial, Helvetica, sans-serif;
+        WHITE-SPACE: nowrap;
+        BACKGROUND-COLOR: #ff9999;
+        FONT-VARIANT: normal
+    }
+
     #layer1 {
         position: absolute;
         width: 562px;
@@ -91,9 +159,14 @@
     .checkbox ul li input[type="checkbox"] {
         opacity: 0;
     }
+
+    .custom-spacing {
+        line-height: 2.9; /* Ajusta el valor según lo necesites */
+    }
 </style>
 
 
+</head>
 
 
 
@@ -120,7 +193,8 @@
 
 
     <div class="card-header">
-        <h4 class="display-4">Mapa</h4>
+        <h1 class="">INGRESE LA UBICACIÓN DE LA SOLICITUD EN EL MAPA</h1>
+        
     </div>
     <div class="card-body">
         <div class="contenedor">
@@ -129,9 +203,53 @@
     -moz-user-select: none; position: relative; border-radius: 15px;" id="dc_main">
 
             </div>
-
+            <div class="custom-spacing">
+                <span class="d-block ms-2  text-primary" tabindex="0" data-bs-toggle="popover" data-bs-trigger="click"
+                    data-bs-html="true" data-bs-content="Si presionas sobre este icono <img src='../mapa/misc/img/alpha_button_fullExtent.png' alt='Icono Mundo' style='width: 5px; height: 5px;'> 
+                                    <strong>Mostar cali </strong> que se encuentra al lado izquierdo de este mensaje
+                                    se enfocará el mapa de cali automaticamente<br> <a href='<?php echo getUrl("Solicitud", "Solicitud", "getImgCategoriaSenial"); ?>' 
+                                    target='_blank'>Ver más</a>">
+                    <i class="bi bi-info-circle mt-3" style="font-size: 1rem; cursor: pointer;"></i>
+                </span>
+                <span class="d-block ms-2 text-primary" tabindex="0" data-bs-toggle="popover" data-bs-trigger="click"
+                    data-bs-html="true" data-bs-content="Si presionas sobre este icono <img src='../mapa/misc/img/alpha_button_pan.png' alt='Icono Mundo' style='width: 5px; height: 5px;'> 
+                                     <strong>Mover mapa</strong> que se encuentra al lado izquierdo de este mensaje
+                                    podras mover el mapa haciendo click sobre el y arrastrandolo a tu preferencia<br>  <a href='<?php echo getUrl("Solicitud", "Solicitud", "getImgCategoriaSenial"); ?>' 
+                                    target='_blank'>Ver más</a>">
+                    <i class="bi bi-info-circle mt-2" style="font-size: 1rem; cursor: pointer;"></i>
+                </span>
+                <span class="d-block ms-2 text-primary" tabindex="0" data-bs-toggle="popover" data-bs-trigger="click"
+                    data-bs-html="true" data-bs-content="Si presionas sobre este icono <img src='../mapa/misc/img/alpha_button_zoombox.png' alt='Icono Mundo' style='width: 5px; height: 5px;'> 
+                                    <strong>Área a acercar</strong> que se encuentra al lado izquierdo de este mensaje 
+                                    podras seleccionar un área del mapa para acercar<br>  <a href='<?php echo getUrl("Solicitud", "Solicitud", "getImgCategoriaSenial"); ?>' 
+                                    target='_blank'>Ver más</a>">
+                    <i class="bi bi-info-circle mt-2" style="font-size: 1rem; cursor: pointer;"></i>
+                </span>
+                <span class="d-block ms-2 text-primary" tabindex="0" data-bs-toggle="popover" data-bs-trigger="click"
+                    data-bs-html="true" data-bs-content="Si presionas sobre este icono  <img src='../mapa/misc/img/alpha_button_zoomin.png' alt='Icono Mundo' style='width: 5px; height: 5px;'> 
+                                     <strong>Acercar</strong> que se encuentra al lado izquierdo de este mensaje
+                                    el mapa se acercará automaticamente sin moverlo ni presionar sobre el<br>  <a href='<?php echo getUrl("Solicitud", "Solicitud", "getImgCategoriaSenial"); ?>' 
+                                    target='_blank'>Ver más</a>">
+                    <i class="bi bi-info-circle mt-2" style="font-size: 1rem; cursor: pointer;"></i>
+                </span>
+                <span class="d-block ms-2 text-primary" tabindex="0" data-bs-toggle="popover" data-bs-trigger="click"
+                    data-bs-html="true" data-bs-content="Si presionas sobre este icono  <img src='../mapa/misc/img/alpha_button_zoomOut.png' alt='Icono Mundo' style='width: 5px; height: 5px;'> 
+                                    <strong>Alejar</strong> que se encuentra al lado izquierdo de este mensaje
+                                    el mapa se alejara automaticamente sin moverlo ni presionar sobre el<br> <a href='<?php echo getUrl("Solicitud", "Solicitud", "getImgCategoriaSenial"); ?>' 
+                                    target='_blank'>Ver más</a>">
+                    <i class="bi bi-info-circle mt-2" style="font-size: 1rem; cursor: pointer;"></i>
+                </span>
+                <span class="d-block ms-2 text-primary" tabindex="0" data-bs-toggle="popover" data-bs-trigger="click"
+                    data-bs-html="true" data-bs-content="Si presionas sobre este icono  <img src='../mapa/misc/img/consultar.png' alt='Icono Mundo' style='width: 5px; height: 5px;'> 
+                                    <strong>Consultar puntos solicitud</strong> que se encuentra al lado izquierdo de este mensaje 
+                                    podras presionar sobre uno de los simbolos del mapa y consultar información sobre una solicitud<br> <a href='<?php echo getUrl("Solicitud", "Solicitud", "getImgCategoriaSenial"); ?>' 
+                                    target='_blank'>Ver más</a>">
+                    <i class="bi bi-info-circle mt-4" style="font-size: 1rem; cursor: pointer;"></i>
+                </span>
+            </div>
 
             <div id="l2">
+
                 <div id="Layer1">
                     <div style="overflow: auto; width: 140px; height: 140px; -moz-user-select: none;
          position: relative; z-index: 200; border-radius: 15px; margin-left: 15px; " id="dc_main2"> </div>
@@ -140,7 +258,7 @@
                 <div id="Layer2">
                     <form name="select_layers" class="checkbox">
                         <ul>
-                            <p align="left">
+                            <!-- <p align="left">
 
                                 <input CHECKED onClick="chgLayers()" type="checkbox" name="layer[0]" value="Cinco">
                                 <strong>Cinco</strong>
@@ -149,29 +267,29 @@
                                 <strong>Six</strong>
                             <p align="left">
                                 <input CHECKED onClick="chgLayers()" type="checkbox" name="layer[2]" value="One">
-                                <strong>One</strong>    
+                                <strong>One</strong>     -->
                             <p align="left">
-                                <input CHECKED onClick="chgLayers()" type="checkbox" name="layer[3]" value="Puntos1">
+                                <input CHECKED onClick="chgLayers()" type="checkbox" name="layer[0]" value="Puntos1">
                                 <strong>Puntos</strong>
 
-                            <!-- <p align="left">
+                            <p align="left">
                                 <input CHECKED onClick="chgLayers()" type="checkbox" name="layer[1]" value="Puntos2">
                                 <strong>Puntos 2</strong>
 
-                             <p align="left">
+                            <p align="left">
                                 <input CHECKED onClick="chgLayers()" type="checkbox" name="layer[2]" value="Puntos3">
                                 <strong>Puntos 3</strong>
-                                <p align="left">
+                            <p align="left">
                                 <input CHECKED onClick="chgLayers()" type="checkbox" name="layer[3]" value="Puntos4">
                                 <strong>Puntos 4</strong>
 
-                                <p align="left">
+                            <p align="left">
                                 <input CHECKED onClick="chgLayers()" type="checkbox" name="layer[4]" value="Puntos5">
                                 <strong>Puntos 5</strong>
 
-                                <p align="left">
+                            <p align="left">
                                 <input CHECKED onClick="chgLayers()" type="checkbox" name="layer[5]" value="Puntos6">
-                                <strong>Puntos 6</strong> -->
+                                <strong>Puntos 6</strong>
                         </ul>
 
 
@@ -210,7 +328,6 @@
 
                 var infola = new msTool('crear punto', infolay, '../mapa/misc/img/seleccionar.png', investiguen);
                 myMap1.getToolbar(0).addMapTool(infola);
-
 
                 chgLayers();
 
@@ -253,9 +370,17 @@
                     }
                 }
 
+
+                var select = false;
+
+                function consulta(e, map) {
+                    map.getTagMap().style.cursor = "crosshair";
+                    select = true;
+                }
                 function investiguen(event, map, x, y, xx, yy) {
                     if (seleccionado) {
-                       
+                        alert("Click sobre las coordenadas : x " + x + "y: " + y + "y reales : x" + xx +
+                            "y: " + yy);
                         //document.getElementById("boton1").click();
 
                         consultar1 = new objectoAjax();
@@ -277,6 +402,7 @@
                         consultar1.onreadystatechange = function() {
                             if (consultar1.readyState == 4) {
                                 var result = consultar1.responseText;
+                                alert(result); //resultado de consulta
                                 window.location.href = "datosMapa.php?x=" + xx + "&y=" + yy + "&id=" + <?php echo $id_solicitud ?>;
                             }
 
@@ -289,7 +415,8 @@
 
 
                 }
-                
+
+               
             </script>
         </div>
 
